cb1e54a61d97f15cfc06df9d56420d54f46e64be
Ross J <ross.jarvis@esynergy.co.uk>
2025-12-18 09:09:31 +0000

    fix(AHWR-1262): updated env vars and added missing message properties #patch (#3)

--- src/messaging/fcp-messaging-service.test.js
+++ src/messaging/fcp-messaging-service.test.js
@@ -6,6 +6,7 @@ import {
   stopMessagingService,
   receivePaymentDataResponseMessages
 } from './fcp-messaging-service.js'
+import { config } from '../config.js'
 
 jest.mock('ffc-ahwr-common-library')
 
@@ -35,12 +36,16 @@ describe('fcp-messaging-service', () => {
   })
 
   describe('sendPaymentRequest', () => {
+    config.set('serviceBus.sendPaymentRequestOutbound', true)
     it('creates and sends message', async () => {
       const mockSendMessage = jest.fn()
       const mockClient = {
         sendMessage: mockSendMessage,
         close: jest.fn(),
         subscribeTopic: jest.fn()
+      }
+      const mockLogger = {
+        info: jest.fn()
       }
       const request = {
         reference: 'IAHW-G3CL-V59P',
@@ -50,14 +55,18 @@ describe('fcp-messaging-service', () => {
       createServiceBusClient.mockReturnValueOnce(mockClient)
 
       await startMessagingService()
-      await sendPaymentRequest(request)
+      await sendPaymentRequest(
+        request,
+        '498064a3-f967-4a98-9d8f-57152e7cbe64',
+        mockLogger
+      )
 
       expect(mockSendMessage).toHaveBeenCalledWith(
         {
           body: request,
           type: 'uk.gov.ffc.ahwr.submit.payment.request',
           source: 'ahwr-payment-proxy',
-          options: {}
+          sessionId: '498064a3-f967-4a98-9d8f-57152e7cbe64'
         },
         'ffc-pay-request'
       )
@@ -72,18 +81,27 @@ describe('fcp-messaging-service', () => {
         close: jest.fn(),
         subscribeTopic: jest.fn()
       }
+      const mockLogger = {
+        info: jest.fn()
+      }
       const request = { category: 'frn', value: '1234567890' }
       createServiceBusClient.mockReturnValueOnce(mockClient)
 
       await startMessagingService()
-      await sendPaymentDataRequest(request)
+      await sendPaymentDataRequest(
+        request,
+        '498064a3-f967-4a98-9d8f-57152e7cbe64',
+        mockLogger,
+        'f1e5a2c4-8d9b-4f73-a1e6-b9d2e0c8a5f4'
+      )
 
       expect(mockSendMessage).toHaveBeenCalledWith(
         {
           body: request,
           type: 'uk.gov.ffc.ahwr.submit.payment.data.request',
           source: 'ahwr-payment-proxy',
-          options: {}
+          sessionId: '498064a3-f967-4a98-9d8f-57152e7cbe64',
+          messageId: 'f1e5a2c4-8d9b-4f73-a1e6-b9d2e0c8a5f4'
         },
         'ffc-pay-data-request'
       )

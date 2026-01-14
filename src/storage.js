5ef4f2bfc51f554109fae997d039105b2e39f8a0
Ross J <ross.jarvis@esynergy.co.uk>
2026-01-14 12:26:55 +0000

    fix(AHWR-1482): update payment blob storage authentication to federated #patch (#24)

--- src/storage.js
+++ src/storage.js
@@ -1,13 +1,34 @@
 import { BlobClient } from '@azure/storage-blob'
-import { DefaultAzureCredential } from '@azure/identity'
+import { ClientAssertionCredential } from '@azure/identity'
 import { streamToBuffer } from './lib/streamToBuffer.js'
+import { config } from './config.js'
+import {
+  CognitoIdentityClient,
+  GetOpenIdTokenForDeveloperIdentityCommand
+} from '@aws-sdk/client-cognito-identity'
+
+const LOGINS = {
+  'ahwr-payment-proxy-aad-access': 'ahwr-payment-proxy'
+}
+
+export const getCognitoToken = async () => {
+  const cognitoClient = new CognitoIdentityClient({
+    region: config.get('aws.region')
+  })
+  const command = new GetOpenIdTokenForDeveloperIdentityCommand({
+    IdentityPoolId: config.get('cognito.poolId'),
+    Logins: LOGINS
+  })
+  const result = await cognitoClient.send(command)
+  return result?.Token
+}
 
 export const createBlobClient = (logger, blobUri) => {
+  const { clientId, tenantId } = config.get('azure')
+
   const blobClient = new BlobClient(
     blobUri,
-    new DefaultAzureCredential({
-      managedIdentityClientId: process.env.AZURE_CLIENT_ID
-    })
+    new ClientAssertionCredential(tenantId, clientId, getCognitoToken)
   )
 
   const getBlob = async () => {

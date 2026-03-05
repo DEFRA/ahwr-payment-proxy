88efeec50773521d07cc5442f287df52bbbc277f
rossjarvisesynergy <ross.jarvis@esynergy.co.uk>
2026-03-05 16:40:45 +0000

    feat(AHWR-1590): support endpoint to get queue messages

--- src/routes/api/support/support-controller.js
+++ src/routes/api/support/support-controller.js
@@ -3,6 +3,8 @@ import Boom from '@hapi/boom'
 import { get } from '../../../repositories/payment-repository.js'
 import { processFrnRequest } from '../../../jobs/request-payment-status.js'
 import { trackEvent } from '../../../common/helpers/logging/logger.js'
+import { SQSClient, ReceiveMessageCommand } from '@aws-sdk/client-sqs'
+import { config } from '../../../config.js'
 
 export const requestPaymentStatusHandler = async (request, h) => {
   try {
@@ -40,5 +42,45 @@ export const requestPaymentStatusHandler = async (request, h) => {
     }
 
     throw Boom.internal(error)
+  }
+}
+
+export const supportQueueMessagesHandler = async (request, h) => {
+  try {
+    const { queueUrl, limit } = request.query
+
+    const client = new SQSClient({
+      region: config.get('aws.region'),
+      endpoint: config.get('aws.endpointUrl')
+    })
+
+    const command = new ReceiveMessageCommand({
+      QueueUrl: queueUrl,
+      MaxNumberOfMessages: limit,
+      VisibilityTimeout: 2,
+      WaitTimeSeconds: 0,
+      AttributeNames: ['All'],
+      MessageAttributeNames: ['All']
+    })
+    const res = await client.send(command)
+
+    request.logger.info(`Retrieved ${res.Messages?.length || 0} messages`)
+
+    const result = (res.Messages || []).map((msg) => ({
+      id: msg.MessageId,
+      body: msg.Body,
+      attributes: msg.Attributes,
+      messageAttributes: msg.MessageAttributes
+    }))
+
+    return h.response(result).code(StatusCodes.OK)
+  } catch (err) {
+    request.logger.error({ err }, 'Failed to get queue messages')
+
+    if (Boom.isBoom(err)) {
+      throw err
+    }
+
+    throw Boom.internal(err)
   }
 }

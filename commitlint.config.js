aa2add33127031e7eb06fffde0025b6017a57a2b
Mathew Attlee <hello@codeinabox.com>
2026-05-21 11:40:37 +0100

    chore: use default rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,17 +1,3 @@
 export default {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'ticket-ref-required': [1, 'always']
-  },
-  plugins: [
-    {
-      rules: {
-        'ticket-ref-required': ({ scope, subject }) => {
-          const ticketPattern = /[A-Z]-\d+/
-          const valid = ticketPattern.test(scope) || ticketPattern.test(subject)
-          return [valid, 'Commit should include a ticket ref eg AHWR-123']
-        }
-      }
-    }
-  ]
+  extends: ['@commitlint/config-conventional']
 }
